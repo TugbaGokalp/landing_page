39ba09cc8abfbede8d4d5b5977d37b3ba600ec0f
TugbaGokalp <tugbagokalp7265@gmail.com>
2024-11-05 09:53:11 +0300

    sections added

--- js/script.js
+++ js/script.js
@@ -12,9 +12,41 @@ fetch("components/homepage.html")
     document.getElementById("content").innerHTML = data;
   });
 
-// Footer dosyasını yükle
-// fetch("footer.html")
-//   .then((response) => response.text())
-//   .then((data) => {
-//     document.getElementById("footer").innerHTML = data;
-//   });
+// Footer
+fetch("components/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("footer").innerHTML = data;
+  });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sliderContainer = document.querySelector(".slider-container");
+  const cardSlider = document.getElementById("cardSlider");
+
+  const cards = document.querySelectorAll(".card");
+
+  console.log("cards", cards);
+
+  if (cards.length === 0) {
+    console.error("Error: '.card' elements not found in the DOM.");
+    return;
+  }
+
+  const cardWidth = cards[0].offsetWidth;
+
+  const leftButton = document.querySelector(".slider-button.left");
+  const rightButton = document.querySelector(".slider-button.right");
+  let scrollPosition = 0;
+
+  rightButton.addEventListener("click", () => {
+    const maxScrollPosition =
+      cardSlider.scrollWidth - sliderContainer.offsetWidth;
+    scrollPosition = Math.min(scrollPosition + cardWidth, maxScrollPosition);
+    cardSlider.style.transform = `translateX(-${scrollPosition}px)`;
+  });
+
+  leftButton.addEventListener("click", () => {
+    scrollPosition = Math.max(scrollPosition - cardWidth, 0);
+    cardSlider.style.transform = `translateX(-${scrollPosition}px)`;
+  });
+});
